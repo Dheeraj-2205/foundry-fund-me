1afecd1a433e75e0229bfe54546cd848ee273e3b
Dheeraj-2205 <djsays124@gmail.com>
2026-03-18 05:04:25 +0530

    adding a test cases in fundme contract

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.26;
+
+import {Test} from "forge-std/Test.sol";
+
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {}
