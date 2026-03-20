79ea0b792d337bfd7b21d0a68f821ca5ee854e4b
Dheeraj-2205 <djsays124@gmail.com>
2026-03-21 02:42:42 +0530

    add the refractor

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.26;
 import {Test, console} from "forge-std/Test.sol";
 
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe();
+        DeployFundMe deployer = new DeployFundMe();
+        fundMe = deployer.run();
     }
 
     function testMinimumDollarIsFive() public {
@@ -17,7 +19,7 @@ contract FundMeTest is Test {
     }
 
     function testOwnerIsMsgSender() public {
-        assertEq(fundMe.i_owner(), address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
     }
 
     function testPriceFeedVersionIsAccurate() public {
